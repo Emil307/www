f6c11dd0759a260e031e5aade0e8027529770bf3
Emil Novikov <emilnovikov392@gmail.com>
2023-07-19 09:58:20 +0600

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1240px;
+  margin: 0 auto;
+`
+
+const Item = styled.div`
+
+`
+
+const Bottom = styled.div``
+
+const Cost = styled.span``
+
+const Button = styled.button``
 
 function App() {
   return (<>
-    <h1>app</h1>
+    <Container>
+      <Item>
+        <img src="" alt="" />
+        <h2>Рубашка</h2>
+        <p>ОЛАУОАыОШАУЫОШауы</p>
+        <div>
+          <span>$450</span>
+          <button>купить</button>
+        </div>
+      </Item>
+    </Container>
   </>)
 }
 
