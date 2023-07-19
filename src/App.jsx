05bf1b57c58e4f762c7a9b034dbb5ecf9e91398e
Emil Novikov <emilnovikov392@gmail.com>
2023-07-19 10:27:48 +0600

    fetching data from jsonplaceholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Container = styled.div`
   width: 100%;
@@ -8,9 +9,13 @@ const Container = styled.div`
 `
 
 const Item = styled.div`
-  padding: 10px;
   border: 1px #000 solid;
   width: 360px;
+`
+
+const Image = styled.img`
+  width: 360px;
+  height: auto;
 `
 
 const Bottom = styled.div``
@@ -20,17 +25,31 @@ const Cost = styled.span``
 const Button = styled.button``
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  async function getPosts() {
+    const API = 'https://jsonplaceholder.typicode.com';
+
+    const response = await axios.get(`${API}/posts`);
+    setPosts(response.data);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
   return (<>
     <Container>
-      <Item>
-        <img src="" alt="" />
-        <h2>Рубашка</h2>
-        <p>ОЛАУОАыОШАУЫОШауы</p>
-        <div>
-          <span>$450</span>
-          <button>купить</button>
-        </div>
-      </Item>
+      {posts.length == 0 ? 
+        <p>упс.... я не нашел ни одного поста за сегодня</p> :
+        <></>
+      }
+
+      {posts.map(post =>
+        <Item>
+          <h1>{post.title}</h1>
+        </Item>  
+      )}
     </Container>
   </>)
 }
